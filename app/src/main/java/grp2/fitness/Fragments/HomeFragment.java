26d3e00352073daf21b3d571e62b7a1a38f9fe08
zerrain <37678931+zerrain@users.noreply.github.com>
2018-05-26 22:07:22 +1000

    Added sorting to leaderboard

--- app/src/main/java/grp2/fitness/Fragments/HomeFragment.java
+++ app/src/main/java/grp2/fitness/Fragments/HomeFragment.java
@@ -20,7 +20,7 @@ import com.amazonaws.models.nosql.DailyDataDO;
 import com.amazonaws.models.nosql.DiaryDO;
 
 import java.util.ArrayList;
-import java.util.Objects;
+import java.util.Comparator;
 
 import grp2.fitness.Handlers.DailyDataManager;
 import grp2.fitness.Handlers.DiaryManager;
@@ -148,6 +148,7 @@ public class HomeFragment extends Fragment implements
 
                 leaderboard.clear();
                 leaderboard.addAll(allDailyData);
+                leaderboard.sort(Comparator.comparing(DailyDataDO::getSteps).reversed());
                 leaderboardAdapter.notifyDataSetChanged();
 
                 for(DailyDataDO dailyDataDO : allDailyData){
